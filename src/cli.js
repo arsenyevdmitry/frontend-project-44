d2f55c6c4c87a6e3df52b273b4833ff62beae699
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2023-09-06 09:54:10 -0300

    asdf

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,8 @@
 
 import readlineSync from 'readline-sync';
 import {
-  getRandomNumber, getMinMaxRandNumber, generateProgression, findGCD, isPrime, getRandomOperation,
+  getRandomNumber, getMinMaxRandNumber, generateProgression,
+  findGCD, isPrimeNumber, getRandomOperation,
 } from './utils.js';
 
 export function runCalcGame() {
@@ -136,12 +137,11 @@ export function startGamePrime() {
 
   for (let i = 0; i < rounds; i++) {
     // Генерируем случайное число от 1 до 100
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    const randomNumber = getMinMaxRandNumber(2, 50);
     console.log(`Question: ${randomNumber}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
-    const isPrimeNumber = isPrime(randomNumber);
-    const correctAnswer = isPrimeNumber ? 'yes' : 'no';
+    const correctAnswer = isPrimeNumber(randomNumber) ? 'yes' : 'no';
 
     if (userAnswer === correctAnswer) {
       console.log(`${userAnswer} \n Correct!`);
